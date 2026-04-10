dda732b97c1bddc012c3dd4f291d807ed5b43afb
Phuc Tran <xuanphuc1209@gmai.com>
2026-04-09 20:23:11 -0500

    added searcher functionalities

--- searcher.py
+++ searcher.py
@@ -4,19 +4,89 @@ import faiss
 from transformers import CLIPProcessor, CLIPModel
 
 class Searcher:
-    def __init__(self, paths_file="paths.json"):
-        self.paths_file = paths_file
-        with open(self.paths_file, "r") as f:
-            self.image_paths = json.load(f)
+    def __init__(self, paths_file="paths.json", index_file="embeddings.faiss"):
+        self.paths_file = paths_file # filename of list of image paths
+        self.index_file = index_file # filename of embedding database
+        self.image_paths = [] # list of image paths
+        self.embedding_index = None # actual embedding database
+
+        # device setup
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device("cpu")
+        print(f"Using {self.device} device")
+
+        # load CLIP model and processor
+        self.model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(self.device)
+        self.processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+
+        self.model.eval()
+
+        # initial sync
+        self.reload_index()
 
     #todo: implement the actual search logic using CLIP and FAISS. For now, it just returns all image paths regardless of the query.
     def search(self, query, top_k=3):
-        print("Recieved query and top_k:", query, top_k)  
-        return self.image_paths
+        print(f"Recieved query='{query}' and top_k={top_k}")
+
+        # base case: if the search box is empty, show everything
+        if not query.strip():
+            return self.image_paths
+    
+        # base case: if the embedding index is empty, show nothing
+        if self.embedding_index is None:
+            return []
+
+        # since we are using the model and not training it, we don't need to track gradients to save memory
+        with torch.no_grad():
+            #todo: I believe we will handle image search one day
+            # call the processor with text
+            inputs = self.processor(text=[query], return_tensors="pt", padding=True)
+            # move to device, safer than self.processor().to(self.device)
+            inputs = { k: v.to(self.device) for k, v in inputs.items() }
+
+            # extract features from the text encoder
+            text_features = self.model.get_text_features(**inputs)
+
+            # normalize the vector to prepare for cosine similarity
+            text_features /= text_features.norm(dim=-1, keepdim=True)
+
+            # text_features is a tensor on self.device
+            # we need to move it back to CPU because FAISS works on CPU
+            # FAISS also works with numpy array and not tensor, so we need to convert back to numpy array
+            query_vector = text_features.cpu().numpy().astype('float32')
+
+        # search nearest k neighbors
+        # dim(indices) = (num_queries, top_k). in this case, num_queries = 1 since we only have 1 query string
+        distances, indices = self.embedding_index.search(query_vector, top_k)
+
+        # map the resulting indices to actual file paths from the metadata
+        # need to remove indices that are outside of valid range
+        results = [self.image_paths[idx] for idx in indices[0] if idx >= 0 and idx < len(self.image_paths)]
+
+        return results
 
     #todo: implement the actual logic to reload the index from disk. For now, it just re-reads the paths file and updates the image_paths list.
     def reload_index(self):
-        with open(self.paths_file, "r") as f:
-            self.image_paths = json.load(f)
+        # loads the image paths from the paths file into memory
+        try:
+            with open(self.paths_file, "r") as f:
+                self.image_paths = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            # handle missing file or corrupted file
+            self.image_paths = []
+            print(f"Warning: {self.paths_file} missing or corrupt.")
+        
+        # loads the embedding database into memory
+        try:
+            self.embedding_index = faiss.read_index(self.index_file)
+            print(f"Index reloaded: {self.embedding_index.ntotal} images available")
+        except Exception:
+            print(f"Warning: {self.index_file} could not be loaded.")
+            # handle error while loading file by adding a dummy index to prevent search crashes
+            self.embedding_index = faiss.IndexFlatIP(512)
 
         print("Search engine reloaded index")
